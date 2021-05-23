d9eca823e673dec49dd24eb8575dec62d87d1df8
Vineeth Kumar <winkumar2000@gmail.com>
2021-05-23 12:11:04 +0530

    Basic Navbar done

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar_header">
+                <div className="navbar_title">
+                    <h1>Welcome</h1>
+                    <h1>Interests</h1>
+                    <h1>Contact</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
