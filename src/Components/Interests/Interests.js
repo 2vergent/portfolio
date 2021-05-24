7318e14d9b11ad29f762bdfa7451f02e15ad4a95
Vineeth Kumar <winkumar2000@gmail.com>
2021-05-24 13:03:01 +0530

    Added a few Interests

--- src/Components/Interests/Interests.js
+++ src/Components/Interests/Interests.js
@@ -7,10 +7,25 @@ const Interest = () => {
             <div className="interests_title">
                 <h1>Interests</h1>
             </div>
-            <div className="skills_section">
-                <h2>Adobe Photoshop</h2>
-                <h2>Adobe After Effects</h2>
-                <h2>Adobe Premiere Pro</h2>
+            <div className="skills">
+                <div className="skills_photo">
+                    <h2>Photo Editing</h2>
+                    <h2>Photo Retouching and Artistic Manipulations</h2>
+                    <h2>Tools - Photoshop and Lightroom</h2>
+                    <h2>Underpixel - Photo Editing page on Instagram</h2>
+                </div>
+                <div className="skills_video">
+                    <h2>Video Editing</h2>
+                    <h2>Making Gaming Montages and Funny Moments Videos</h2>
+                    <h2>Tools - After Effects and Premiere Pro</h2>
+                    <h2>sphn1x - Youtube Channel</h2>
+                </div>
+                <div className="skills_hobbies">
+                    <h2>Hobbies</h2>
+                    <h2>Coding</h2>
+                    <h2>Watching Movies</h2>
+                    <h2>Making Youtube videos</h2>
+                </div>
             </div>
         </div>
     )
