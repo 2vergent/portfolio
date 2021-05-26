314ac3d8b30909970aa8bd6d9b1a0f2e05734a48
Vineeth Kumar <winkumar2000@gmail.com>
2021-05-26 10:22:15 +0530

    It sucked, so I started all over again

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar'
 import Welcome from './Components/Welcome/Welcome'
-import Interests from './Components/Interests/Interests'
-import Contact from './Components/Contact/Contact'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Welcome />
-      <Interests />
-      <Contact />
     </div>
   );
 }
--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -11,24 +11,27 @@ import InstaLogoColor from '../../Assets/InstagramLogoColor.jpg'
 
 const Welcome = () => {
     return (
-        <div className="welcome">
-            <div className="pic_name text-sm md:text-lg">
-                <img src={DP} alt="DP" className="avatar"></img>
-                <h1>Vineeth Kumar</h1>
-                <h2>Avid Coder &nbsp;&nbsp;|&nbsp;&nbsp; Tech Enthusiast &nbsp;&nbsp;|&nbsp;&nbsp; Photo Editor</h2>
-                <div className="links">
-                    <a href="https://github.com/2vergent">
-                        <img src={GitLogo} alt="GithubLogo" className="link_github" />
-                        <img src={GitLogoColor} alt="GithubLogoColor" className="link_github" />
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCrY2EjtxWozHOP2pflkM9JQ">
-                        <img src={YTLogo} alt="YoutubeLogo" className="link_youtube" />
-                        <img src={YTLogoColor} alt="YoutubeLogoColor" className="link_youtube" />
-                    </a>
-                    <a href="https://www.instagram.com/underpixel/">
-                        <img src={InstaLogo} alt="InstagramLogo" className="link_instagram" />
-                        <img src={InstaLogoColor} alt="InstagramLogoColor" className="link_instagram" />
-                    </a>
+        <div className="main_welcome">
+            <h1>Welcome</h1>
+            <div className="welcome">
+                <div className="pic_name text-sm md:text-lg">
+                    <img src={DP} alt="DP" className="avatar"></img>
+                    <h1>Vineeth Kumar</h1>
+                    <h2>Avid Coder &nbsp;&nbsp;|&nbsp;&nbsp; Tech Enthusiast &nbsp;&nbsp;|&nbsp;&nbsp; Photo Editor</h2>
+                    <div className="links">
+                        <a href="https://github.com/2vergent">
+                            <img src={GitLogo} alt="GithubLogo" className="link_github" />
+                            <img src={GitLogoColor} alt="GithubLogoColor" className="link_github" />
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCrY2EjtxWozHOP2pflkM9JQ">
+                            <img src={YTLogo} alt="YoutubeLogo" className="link_youtube" />
+                            <img src={YTLogoColor} alt="YoutubeLogoColor" className="link_youtube" />
+                        </a>
+                        <a href="https://www.instagram.com/underpixel/">
+                            <img src={InstaLogo} alt="InstagramLogo" className="link_instagram" />
+                            <img src={InstaLogoColor} alt="InstagramLogoColor" className="link_instagram" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
